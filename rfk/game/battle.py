59f0079c974157114546456b587ba68bd2d55fd2
bustedchickn <talmage.kay@gmail.com>
2021-11-30 15:29:58 -0700

    ripping rfk game

---
+++ rfk/game/battle.py
@@ -0,0 +1,1 @@
+#this is where the battle stuff is going to be
---
+++ rfk/game/draw_actors_action.py
@@ -0,0 +1,48 @@
+from game.action import Action
+
+# TODO: Define the DrawActorsAction class here
+class DrawActorsAction(Action):
+    """A code template for controlling actors. The responsibility of this
+    class of objects is translate user input into some kind of intent.
+    
+    Stereotype:
+        Controller
+
+    Attributes:
+        _input_service (InputService): An instance of InputService.
+    """
+
+    def __init__(self, output_service):
+        """The class constructor.
+        
+        Args:
+            input_service (InputService): An instance of InputService.
+        """
+        self._output_service = output_service
+        
+
+    def execute(self, cast):
+        """Executes the action using the given actors.
+
+        Args:
+            cast (dict): The game actors {key: tag, value: list}.
+        """
+        self._output_service.clear_screen()
+        
+        marquee = cast["marquee"][0]
+        self._output_service.draw_actor(marquee)
+
+        robot = cast["robot"][0]
+        self._output_service.draw_actor(robot)
+
+
+
+        
+        artifacts = cast["artifact"]
+        self._output_service.draw_actors(artifacts)
+
+        
+
+
+
+        self._output_service.flush_buffer()
